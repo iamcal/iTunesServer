05e770f93da2e08e3d9e81da4343580a03b696ed
Cal Henderson <cal@iamcal.com>
2010-08-25 03:59:10 +0000

    a basic authentication library
    git-svn-id: https://svn.iamcal.com/public/php/iTunesServer/trunk@5768 ac7c8259-0de0-4f3c-bb6a-12394caf8efd

--- include/init.php
+++ include/init.php
@@ -17,6 +17,7 @@
 	include(INCLUDE_DIR.'/lib_misc.php');
 	include(INCLUDE_DIR.'/lib_lastfm.php');
 	include(INCLUDE_DIR.'/lib_http.php');
+	include(INCLUDE_DIR.'/lib_auth.php');
 
 
 	#
@@ -28,28 +29,5 @@
 		echo "<pre style=\"text-align: left;\">";
 		echo HtmlSpecialChars(var_export($foo, 1));
 		echo "</pre>\n";
-	}
-
-
-	function auth_create_token($name){
-
-		$time = time();
-		$base = "{$name}-{$time}";
-		$sig = sha1($GLOBALS[cfg][token_secret].$base);
-		return "{$base}-{$sig}";
-	}
-
-	function auth_check_token($token){
-
-		$bits = explode('-', $token);
-		$sig = array_pop($bits);
-		$time = array_pop($bits);
-		$name = implode('-', $bits);
-
-		$test = sha1($GLOBALS[cfg][token_secret]."{$name}-{$time}");
-
-		if ($test != $sig) return 0;
-
-		return $name;
 	}
 ?>
---
+++ include/lib_auth.php
@@ -0,0 +1,107 @@
+<?
+	#
+	# $Id$
+	#
+
+	$cfg[user] = null;
+
+	if ($cfg[allow_authed_users]){
+		auth_check();
+	}
+
+	##############################################################
+	#
+	# check to see if anyone is authed
+	#
+
+	function auth_check(){
+
+		if (!$_COOKIE[u]) return;
+		list($id, $hash) = explode('-', $_COOKIE[u]);
+
+		$id = intval($id);
+		$row = db_fetch_hash(db_query("SELECT * FROM users WHERE id=$id"));
+
+		if ($row[id] && $row[password] == $hash){
+
+			$GLOBALS[cfg][user] = $row;
+		}
+	}
+
+	function auth_set_login($row){
+
+		$value = "$row[id]-$row[password]";
+		$expire = time() + (10 * 365 * 24 * 60 * 60);
+
+		setcookie('u', $value, $expire, $GLOBALS[cfg][cookie_path], $GLOBALS[cfg][cookie_domain]);
+	}
+
+	##############################################################
+
+	function auth_hash_password($password){
+
+		$salt = substr(md5(time() . $password), 0, 2);
+		return $salt.sha1($password.$salt);
+	}
+
+	function auth_compare_password($password, $db_hash){
+
+		#
+		# the first 2 bytes are the salt
+		#
+
+		$salt = substr($db_hash, 0, 2);
+		$hash = substr($db_hash, 2);
+
+		if (sha1($password.$salt) == $hash){
+
+			return true;
+		}
+
+		return false;
+	}
+
+	##############################################################
+
+	function auth_ensure_loggedout(){
+
+		if ($GLOBALS[cfg][user][id]){
+			header('location: ./');
+			exit;
+		}
+	}
+
+	function auth_ensure_loggedin(){
+
+		if (!$GLOBALS[cfg][user][id]){
+			header('location: login.php');
+			exit;
+		}
+	}
+
+	##############################################################
+
+	function auth_create_token($name){
+
+		$time = time();
+		$base = "{$name}-{$time}";
+		$sig = sha1($GLOBALS[cfg][token_secret].$base);
+		return "{$base}-{$sig}";
+	}
+
+	function auth_check_token($token){
+
+		$bits = explode('-', $token);
+		$sig = array_pop($bits);
+		$time = array_pop($bits);
+		$name = implode('-', $bits);
+
+		$test = sha1($GLOBALS[cfg][token_secret]."{$name}-{$time}");
+
+		if ($test != $sig) return 0;
+
+		return $name;
+	}
+
+	##############################################################
+?>
